b6543ba595ea92175e5dfeba581aa4183130f04f
youngil <youngil@placeb.com>
2023-06-04 14:10:59 +0900

    MuteSwitchDetector 추가

---
+++ Assets/MuteSwitchDetector/Example/MuteSwitchText.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MuteSwitchText : MonoBehaviour {
+
+	private Text textUI;
+	private string initialText;
+
+	public MuteSwitchDetector detector;
+
+	void Start () {
+		textUI = GetComponent<Text>();
+		initialText = textUI.text;
+
+		#if UNITY_IPHONE && !UNITY_EDITOR
+		if (detector != null) {
+			textUI.text = "";
+			detector.onMuteSwitchChanged.AddListener(OnMuteSwitchChanged);
+		}
+
+		#else 
+		textUI.gameObject.SetActive(false);
+		#endif
+	}
+
+	void OnMuteSwitchChanged(bool newState) {
+		DisplayMuteSwitchState(newState);
+	}
+
+	void DisplayMuteSwitchState(bool state) {
+		textUI.text = initialText + state;
+	}
+}
---
+++ Assets/MuteSwitchDetector/MuteSwitchDetector.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine.Events;
+
+public class MuteSwitchDetector : MonoBehaviour {
+
+	[System.Serializable]
+	public class MuteSwitchChangedEvent : UnityEvent<bool>
+	{
+	}
+	
+	#if UNITY_IPHONE && !UNITY_EDITOR
+	[DllImport ("__Internal")]
+	private static extern void StartListeningForMuteChanges();
+
+	[DllImport ("__Internal")]
+	private static extern void StopListeningForMuteChanges();
+
+	[DllImport ("__Internal")]
+	private static extern bool IsMuted();
+	#endif
+
+	public MuteSwitchChangedEvent onMuteSwitchChanged;
+
+	public bool isMuted {
+		get {
+			#if UNITY_IPHONE && !UNITY_EDITOR
+			return IsMuted();
+			#endif
+			return false;
+		}
+	}
+
+	void Start () {
+
+		if (onMuteSwitchChanged == null) {
+			onMuteSwitchChanged = new MuteSwitchChangedEvent();
+		}
+
+		#if UNITY_IPHONE && !UNITY_EDITOR
+		StartListeningForMuteChanges();
+		#endif
+	}
+		
+	void OnDestroy() {
+		#if UNITY_IPHONE && !UNITY_EDITOR
+		StopListeningForMuteChanges();
+		#endif
+	}
+
+	void OnMuteStateChanged(string message) {
+		bool newState = bool.Parse(message);
+		onMuteSwitchChanged.Invoke(newState);
+	}
+}
